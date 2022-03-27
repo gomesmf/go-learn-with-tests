b297ee96e078223176c7771099f5617b9e506a12
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-03-27 14:58:47 -0300

    Add Withdraw function

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,15 +5,42 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
-	wallet.Deposit(Bitcoin(10))
-
-	got := wallet.Balance()
-	// fmt.Printf("address of balance in test is %v\n", &wallet.balance)
-
-	want := Bitcoin(10)
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but did not get one")
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err)
+		assertBalance(t, wallet, startingBalance)
+	})
 }
