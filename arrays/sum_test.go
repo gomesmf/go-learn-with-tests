b8d016d2531c608cc29cf665f93c32cd7f9b7765
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-03-27 01:05:07 -0300

    Add SumAllTails

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -31,3 +31,12 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v expected %v", got, expected)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	expected := []int{2, 9}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("got %v expected %v", got, expected)
+	}
+}
