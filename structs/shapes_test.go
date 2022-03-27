3ed45230dd172ff5d47a00bbfd52ef302d97658b
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-03-27 12:48:44 -0300

    Add test driven test

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,21 +13,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, expected float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != expected {
-			t.Errorf("got %g want %g", got, expected)
-		}
+	areaTests := []struct {
+		shape    Shape
+		expected float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 12.0}
-		checkArea(t, rectangle, 144.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.expected {
+			t.Errorf("got %g expected %g", got, tt.expected)
+		}
+	}
 }
