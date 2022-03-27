3096c87aacb41c51b1758807066defcccdf26b0d
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-03-27 12:45:27 -0300

    Add Shape interface

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,7 +3,8 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	expected := 40.0
 
 	if got != expected {
@@ -12,10 +13,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	expected := 100.0
-
-	if got != expected {
-		t.Errorf("got %.2f expected %.2f", got, expected)
+	checkArea := func(t testing.TB, shape Shape, expected float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != expected {
+			t.Errorf("got %g want %g", got, expected)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 12.0}
+		checkArea(t, rectangle, 144.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
