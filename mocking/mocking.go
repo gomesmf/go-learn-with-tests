f752101504b1f2166ddb2d88104a52e34d977f7d
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-03-28 13:57:16 -0300

    Add SpyCountdownOperations implementing io.Writer and Sleeper interfaces

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -37,6 +37,22 @@ func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+const sleep = "sleep"
+const write = "write"
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func main() {
 	sleeper := &DefaultSleeper{}
 	Countdown(os.Stdout, sleeper)
