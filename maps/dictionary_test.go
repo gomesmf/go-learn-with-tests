179f31bf2117e0e63f675f28f34c9390cfad8796
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-03-27 22:37:05 -0300

    Add Update method

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -70,3 +70,25 @@ func assertDefinition(t testing.TB, dictionary Dictionary, word, definition stri
 		t.Errorf("got %q want %q", got, definition)
 	}
 }
+
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new test"
+		dictionary.Update(word, newDefinition)
+
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
