9782b1339dbb3bde4fc0bb176c65b58b851b66ce
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-04-06 15:35:52 -0300

    Add TestHoursInRadians

--- math/clockface.go
+++ math/clockface.go
@@ -38,6 +38,10 @@ func minutesInRadians(t time.Time) float64 {
 	return (secondsInRadians(t) / 60) + (math.Pi / (30 / float64(t.Minute())))
 }
 
+func hoursInRadians(t time.Time) float64 {
+	return (minutesInRadians(t) / 12) + (math.Pi / (6 / float64(t.Hour()%12)))
+}
+
 func SVGWriter(w io.Writer, t time.Time) {
 	io.WriteString(w, svgStart)
 	io.WriteString(w, bezel)
