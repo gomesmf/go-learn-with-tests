f608b10ae05016fefd61bf088fd0f276c982f3bb
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-04-06 15:06:03 -0300

    Refactor

--- math/clockface_test.go
+++ math/clockface_test.go
@@ -1,8 +1,6 @@
 package clockface
 
 import (
-	"bytes"
-	"encoding/xml"
 	"math"
 	"testing"
 	"time"
@@ -48,6 +46,25 @@ func TestSecondHandPoint(t *testing.T) {
 	}
 }
 
+func TestMinutesInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 30, 0), math.Pi},
+		{simpleTime(0, 0, 7), 7 * (math.Pi / (30 * 60))},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := minutesInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+}
+
 func roughlyEqualFloat64(a, b float64) bool {
 	const equalityThreshold = 1e-7
 	return math.Abs(a-b) < equalityThreshold
@@ -64,88 +81,4 @@ func simpleTime(hours, minutes, seconds int) time.Time {
 
 func testName(t time.Time) string {
 	return t.Format("15:04:05")
-}
-
-type SVG struct {
-	XMLName xml.Name `xml:"svg"`
-	Xmlns   string   `xml:"xmlns,attr"`
-	Width   string   `xml:"width,attr"`
-	Height  string   `xml:"height,attr"`
-	ViewBox string   `xml:"viewBox,attr"`
-	Version string   `xml:"version,attr"`
-	Circle  Circle   `xml:"circle"`
-	Line    []Line   `xml:"line"`
-}
-
-type Circle struct {
-	Cx float64 `xml:"cx,attr"`
-	Cy float64 `xml:"cy,attr"`
-	R  float64 `xml:"r,attr"`
-}
-
-type Line struct {
-	X1 float64 `xml:"x1,attr"`
-	Y1 float64 `xml:"y1,attr"`
-	X2 float64 `xml:"x2,attr"`
-	Y2 float64 `xml:"y2,attr"`
-}
-
-func TestSVGWriterAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-	b := bytes.Buffer{}
-
-	SVGWriter(&b, tm)
-
-	svg := SVG{}
-
-	xml.Unmarshal(b.Bytes(), &svg)
-
-	want := Line{150, 150, 150, 60}
-
-	for _, line := range svg.Line {
-		if line == want {
-			return
-		}
-	}
-
-	t.Errorf("expected to find the second hand line %+v in the SVG lines %+v", want, svg.Line)
-}
-
-func TestSVGWriterSecondHand(t *testing.T) {
-	cases := []struct {
-		time time.Time
-		line Line
-	}{
-		{
-			time: simpleTime(0, 0, 0),
-			line: Line{150, 150, 150, 60},
-		},
-		{
-			time: simpleTime(0, 0, 30),
-			line: Line{150, 150, 150, 240},
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(testName(c.time), func(t *testing.T) {
-			b := bytes.Buffer{}
-			SVGWriter(&b, c.time)
-
-			svg := SVG{}
-			xml.Unmarshal(b.Bytes(), &svg)
-
-			if !containsLine(c.line, svg.Line) {
-				t.Errorf("expected to find the second hand line %+v in the SVG lines %+v", c.line, svg.Line)
-			}
-		})
-	}
-}
-
-func containsLine(l Line, ls []Line) bool {
-	for _, line := range ls {
-		if line == l {
-			return true
-		}
-	}
-	return false
 }
