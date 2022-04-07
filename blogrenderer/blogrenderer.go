d4c7eaa9a2c8486df4b98f6a45c20ffc0d9752ab
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-04-07 16:09:33 -0300

    Add blogrenderer

---
+++ blogrenderer/blogrenderer.go
@@ -0,0 +1,30 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplate embed.FS
+)
+
+type Post struct {
+	Title, Body, Description string
+	Tags                     []string
+}
+
+func Render(w io.Writer, p Post) error {
+	templ, err := template.ParseFS(postTemplate, "templates/*.gohtml")
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
