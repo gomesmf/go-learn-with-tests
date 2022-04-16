8e59e8566ab47a28f0f4a30fae514607e318d085
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-04-16 18:00:05 -0300

    Add sorting functionality to GetLeague method

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestFileSystemStore(t *testing.T) {
-	t.Run("league from reader", func(t *testing.T) {
+	t.Run("league sorted", func(t *testing.T) {
 		database, cleanDatabase := createTempFile(t, `[
 {"Name": "Cleo", "Wins": 10},
 {"Name": "Chris", "Wins": 33}]`)
@@ -20,8 +20,8 @@ func TestFileSystemStore(t *testing.T) {
 		got := store.GetLeague()
 
 		want := League{
-			{"Cleo", 10},
 			{"Chris", 33},
+			{"Cleo", 10},
 		}
 
 		assertLeague(t, got, want)
