d922a587d1f42d26b200587e4b0e969607bf4549
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-04-19 14:12:29 -0300

    Add steadily increasing blind value functionality

--- http-server/CLI.go
+++ http-server/CLI.go
@@ -22,8 +22,18 @@ func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 }
 
 func (cli *CLI) PlayPoker() {
+	cli.scheduledBlindAlerts()
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
+}
+
+func (cli *CLI) scheduledBlindAlerts() {
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		cli.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Minute
+	}
 }
 
 func extractWinner(userInput string) string {
@@ -33,8 +43,4 @@ func extractWinner(userInput string) string {
 func (cli *CLI) readLine() string {
 	cli.in.Scan()
 	return cli.in.Text()
-}
-
-type BlindAlerter interface {
-	ScheduleAlertAt(duration time.Duration, amount int)
 }
