9da396159e0736be565ec4f9c60b4ebcf81122bf
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-04-19 16:09:57 -0300

    Remove obsolete test cases from CLI_test.go

--- http-server/CLI_test.go
+++ http-server/CLI_test.go
@@ -2,8 +2,6 @@ package poker_test
 
 import (
 	"bytes"
-	"fmt"
-	"time"
 
 	poker "github.com/gomesmf/go-learning/http-server"
 
@@ -38,77 +36,5 @@ func TestCLI(t *testing.T) {
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Cleo")
-	})
-
-	t.Run("it schedules printing of blind values", func(t *testing.T) {
-		in := strings.NewReader("5\nChris wins\n")
-		playerStore := &poker.StubPlayerStore{}
-		blindAlerter := &poker.SpyBlindAlerter{}
-
-		game := poker.NewGame(blindAlerter, playerStore)
-		cli := poker.NewCLI(in, dummyStdOut, game)
-		cli.PlayPoker()
-
-		cases := []poker.ScheduledAlert{
-			{0 * time.Second, 100},
-			{10 * time.Minute, 200},
-			{20 * time.Minute, 300},
-			{30 * time.Minute, 400},
-			{40 * time.Minute, 500},
-			{50 * time.Minute, 600},
-			{60 * time.Minute, 800},
-			{70 * time.Minute, 1000},
-			{80 * time.Minute, 2000},
-			{90 * time.Minute, 4000},
-			{100 * time.Minute, 8000},
-		}
-
-		for i, want := range cases {
-			t.Run(fmt.Sprint(want), func(t *testing.T) {
-
-				if len(blindAlerter.Alerts) <= i {
-					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.Alerts)
-				}
-
-				got := blindAlerter.Alerts[i]
-				poker.AssertScheduledAlert(t, got, want)
-			})
-		}
-	})
-
-	t.Run("it prompts the user to enter the number of players", func(t *testing.T) {
-		stdout := &bytes.Buffer{}
-		in := strings.NewReader("7\n")
-		blindAlerter := &poker.SpyBlindAlerter{}
-
-		game := poker.NewGame(blindAlerter, dummyPlayerStore)
-		cli := poker.NewCLI(in, stdout, game)
-		cli.PlayPoker()
-
-		got := stdout.String()
-		want := poker.PlayerPrompt
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-
-		cases := []poker.ScheduledAlert{
-			{0 * time.Second, 100},
-			{12 * time.Minute, 200},
-			{24 * time.Minute, 300},
-			{36 * time.Minute, 400},
-		}
-
-		for i, want := range cases {
-			t.Run(fmt.Sprint(want), func(t *testing.T) {
-
-				if len(blindAlerter.Alerts) <= i {
-					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.Alerts)
-				}
-
-				got := blindAlerter.Alerts[i]
-				poker.AssertScheduledAlert(t, got, want)
-			})
-		}
 	})
 }
