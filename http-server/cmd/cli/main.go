70f0aa683e48dc0acf8b6305172e8f366948885d
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-04-19 15:55:55 -0300

    Adjust cmd/cli/main.go

--- http-server/cmd/cli/main.go
+++ http-server/cmd/cli/main.go
@@ -20,5 +20,7 @@ func main() {
 
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
-	poker.NewCLI(store, os.Stdin, os.Stdout, poker.BlindAlerterFunc(poker.StdOutAlerter)).PlayPoker()
+
+	game := poker.NewGame(poker.BlindAlerterFunc(poker.StdOutAlerter), store)
+	poker.NewCLI(os.Stdin, os.Stdout, game).PlayPoker()
 }
