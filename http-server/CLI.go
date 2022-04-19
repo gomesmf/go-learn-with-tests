18272932449cd061e2c7d2c99351d2cc3e766b85
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-04-19 15:38:53 -0300

    Add support for entering number of players

--- http-server/CLI.go
+++ http-server/CLI.go
@@ -2,37 +2,50 @@ package poker
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"strconv"
 	"strings"
 	"time"
 )
 
+const PlayerPrompt = "Please enter the number of players: "
+
 type CLI struct {
 	playerStore PlayerStore
 	in          *bufio.Scanner
+	out         io.Writer
 	alerter     BlindAlerter
 }
 
-func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
+func NewCLI(store PlayerStore, in io.Reader, out io.Writer, alerter BlindAlerter) *CLI {
 	return &CLI{
 		playerStore: store,
 		in:          bufio.NewScanner(in),
+		out:         out,
 		alerter:     alerter,
 	}
 }
 
 func (cli *CLI) PlayPoker() {
-	cli.scheduledBlindAlerts()
+	fmt.Fprint(cli.out, PlayerPrompt)
+
+	numberOfPlayers, _ := strconv.Atoi(cli.readLine())
+
+	cli.scheduledBlindAlerts(numberOfPlayers)
+
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
 }
 
-func (cli *CLI) scheduledBlindAlerts() {
+func (cli *CLI) scheduledBlindAlerts(numberOfPlayers int) {
+	blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
+
 	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
 	blindTime := 0 * time.Second
 	for _, blind := range blinds {
 		cli.alerter.ScheduleAlertAt(blindTime, blind)
-		blindTime = blindTime + 10*time.Minute
+		blindTime = blindTime + blindIncrement
 	}
 }
 
